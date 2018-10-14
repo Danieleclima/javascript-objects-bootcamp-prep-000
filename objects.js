8de8064eb4bc9b43736f5f2ec74afcd17b180fab
Daniel Lima <daniel.echuaca.lima@gmail.com>
2018-10-14 00:39:39 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,6 +1,6 @@
 var playlist = {De La: "Come Out And See", The Game: "Dreams"}
 
 function updatePlaylist(playlist, artistName, songTitle){
-  playlist.artistName = songTitle;
-  return playlist;
+  playlist.artistName = songTitle
+  return playlist
 }
